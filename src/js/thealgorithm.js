b647da3e6c4a1ce719bcb7a40a5e94d49056fbfa
Rauri Rochford <raurir@gmail.com>
2016-04-03 14:44:19 +1000

    using timbre instead

--- src/js/thealgorithm.js
+++ src/js/thealgorithm.js
@@ -1,5 +1,7 @@
 var con = console;
 
+var audioPath = "wav/"; 
+
 var sounds = [
 	{index: 0, id: "danke", src:"danke-tambourine.wav"},
 	{index: 1, id: "cod2pac", src:"cod2pac-2-hara-luu-loop-chippy-120.wav"},
@@ -7,60 +9,76 @@ var sounds = [
 	{index: 3, id: "theshivaeffect", src:"theshivaeffect-interesting-drum-pattern.wav"},
 ];
 
-var stage = new createjs.Stage("demoCanvas");
-var circle = new createjs.Shape();
-circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-circle.x = 100;
-circle.y = 100;
-stage.addChild(circle);
-
-
-stage.update();
-
-function updateEffect(value) {
-	con.log("updateEffect", value);
-	stage.update();
+var createInput = (name, min, max, handler) => {
+	var div = document.createElement("div");
+	var label = document.createElement("label");
+	label.innerHTML = name;
+	var input = document.createElement("input");
+	input.type = "range";
+	input.min = min;
+	input.max = max;
+	document.body.appendChild(div);
+	div.appendChild(label);
+	div.appendChild(input);
+	input.addEventListener("change", handler);
 }
 
-function init() {
-	// if initializeDefaultPlugins returns false, we cannot play sound in this browser
-	if (!createjs.Sound.initializeDefaultPlugins()) {return;}
-	var audioPath = "wav/"; 
-//   createjs.Sound.alternateExtensions = ["mp3"];
-	createjs.Sound.addEventListener("fileload", handleLoad);
-	createjs.Sound.registerSounds(sounds, audioPath);
-}
- 
-var index = 0;
-function handleLoad(event) {
-	con.log("handleLoad", event);
-	var instance = createjs.Sound.play(event.id, {
-		interrupt: createjs.Sound.INTERRUPT_ANY,
-		loop: -1
-	});
-
-	instance.on("complete", handleComplete, this);
-	instance.volume = 0.5;
-
-	index ++;
-
-	var slider = new Slider(0, 1, 200, 50).set({x: 200, y: index * 50, value: 25});
-	slider.on("change", handleSliderChange, this);
-	stage.addChild(slider);
-	stage.update();
 
 
-	function handleSliderChange(evt) {
-		updateEffect(evt.target.value);
-		instance.volume = evt.target.value;
+var channels = [], loaded = 0;
+
+
+// createInput("delay", 0, 100, (e) => {
+// 	delay.set({mul: e.currentTarget.value / 100});
+// });
+// createInput("hiss", 0, 100, (e) => {
+// 	hiss.set({mul: e.currentTarget.value / 100});
+// });
+
+
+function loadSound(i) {
+
+	var file = audioPath + sounds[i].src;
+	var soundID = sounds[i].id;
+
+	var channel ;
+	var filter = i % 2 == 0 ? "lpf" : "hpf";
+
+	let handleFilter = (e) => {
+		var cutoff = parseFloat(e.currentTarget.value);
+		con.log("handleFilter", soundID, cutoff);
+		channel.set({cutoff: cutoff})
+	};
+
+	let handleVolume = (e) => {
+		var volume = parseFloat(e.currentTarget.value / 100);
+		con.log("handleVolume", soundID, volume);
+		channel.set({mul: volume})
+	};
+
+	function loadComplete(res) {
+		loaded++;
+		con.log("loaded!", i, loaded);
+
+		createInput("cutoff " + soundID, 0, 10000, handleFilter);
+		createInput("vol " + soundID, 0, 100, handleVolume);
+
+		if (loaded === sounds.length) {
+			T("+", channels).play();
+		}
+
 	}
 
-	updateEffect(slider.value);
+	var channel = T(filter, {cutoff: 1200},
+		T("audio", {loop: true}).loadthis(file, loadComplete)
+	);
+
+	channels[i] = channel;
 
 }
+for (var i = sounds.length - 1; i >= 0; i--) {
+	loadSound(i);
+};
 
-function handleComplete(event) {
-	con.log("handleComplete", event);
-}
 
-init();
+// init();
